b0dfc74cfbd1e8133df3ab3cadf851a2502f7712
koray-eren <koray.eren@outlook.com>
2022-02-27 17:26:34 +1100

    added dark mode toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Button from '@mui/material/Button';
+import { ThemeProvider } from '@mui/material';
+import { createTheme, ThemeOptions } from '@mui/material/styles';
+
+import Brightness3Icon from '@mui/icons-material/Brightness3';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+
+import CssBaseline from "@mui/material/CssBaseline";
+
+export const light = {
+  palette: {
+    mode: 'light',
+  },
+} as ThemeOptions
+export const dark = {
+  palette: {
+    mode: 'dark',
+  },
+} as ThemeOptions
 
 function App() {
+  const [theme, setTheme] = useState(true);
+  
+  const appliedTheme = createTheme(theme ? light : dark)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={appliedTheme}>
+      <CssBaseline />
+      <div className="App">
+        <p>The theme is {theme ? "light" : "dark" }</p>
+          <Button onClick={() => setTheme(!theme)} color="inherit">
+            {!theme ? 
+            <>
+              Light Mode
+              <Brightness7Icon />
+            </> 
+            : 
+            <>
+              Dark Mode
+              <Brightness3Icon />
+            </> }
+          </Button>
+      </div>
+    </ThemeProvider>
   );
 }
 
 export default App;
+
